7565efee2076d760db1f68d8fbce5de97d474547
wuxun.ren <wuxun.ren@wenba100.com>
2017-11-01 10:24:54 +0800

    self::=>static::

--- src/PHPServer/Log.php
+++ src/PHPServer/Log.php
@@ -32,33 +32,33 @@ class PHPServer_Log {
     }
 
     protected static function log($msg, $level, $context = array()) {
-        $msg = self::format($msg, $level, $context);
-        file_put_contents(self::logFile(), $msg.PHP_EOL, FILE_APPEND);
+        $msg = static::format($msg, $level, $context);
+        file_put_contents(static::logFile(), $msg.PHP_EOL, FILE_APPEND);
     }
 
     public static function debug($msg, $context = array()) {
-        self::log($msg, self::LEVEL_DEBUG, $context);
+        static::log($msg, static::LEVEL_DEBUG, $context);
     }
     public static function info($msg, $context = array()) {
-        self::log($msg, self::LEVEL_INFO, $context);
+        static::log($msg, static::LEVEL_INFO, $context);
     }
     public static function notice($msg, $context = array()) {
-        self::log($msg, self::LEVEL_NOTICE, $context);
+        static::log($msg, static::LEVEL_NOTICE, $context);
     }
     public static function warning($msg, $context = array()) {
-        self::log($msg, self::LEVEL_WARNING, $context);
+        static::log($msg, static::LEVEL_WARNING, $context);
     }
     public static function error($msg, $context = array()) {
-        self::log($msg, self::LEVEL_ERROR, $context);
+        static::log($msg, static::LEVEL_ERROR, $context);
     }
     public static function crit($msg, $context = array()) {
-        self::log($msg, self::LEVEL_CRIT, $context);
+        static::log($msg, static::LEVEL_CRIT, $context);
     }
     public static function alert($msg, $context = array()) {
-        self::log($msg, self::LEVEL_ALERT, $context);
+        static::log($msg, static::LEVEL_ALERT, $context);
     }
     public static function emerg($msg, $context = array()) {
-        self::log($msg, self::LEVEL_EMERG, $context);
+        static::log($msg, static::LEVEL_EMERG, $context);
     }
 
     public static function aLine($s) {
